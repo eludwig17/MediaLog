f6e42d950b59a9ab8cf0cd028e076b25b8e0c01a
Anthony Reynafarje <anthony.reynafarje@eagles.cui.edu>
2026-04-16 14:42:17 -0700

    First draft of Routing

--- backend/models/library.py
+++ backend/models/library.py
@@ -1,7 +1,36 @@
-import DbApi from "../models"
+import DBAPI from "../database"
 class Library:
 
-    @app.route("/api/books")
-    def getbooks(self):
-        data = DbApi.Query("SELECT * FROM books")
-        return jsonify(data, 201)
+    @app.route("/api/books/<int:isbn>")
+    def getbooks(isbn):
+        data = DBAPI.GetBookByISBN(isbn)
+        return jsonify(data),200
+    @app.route("/api/books/add", methods=['POST'])
+    def add_book(book):
+        book = request.get_json()
+        result = DBAPI.InsertBook(
+            book["isbn"],
+            book["Title"],
+            book["AuthorUID"],
+            book["PublishYear"],
+            book["PublishUID"],
+            book["Locale"])
+        return jsonify({"success": True, "result": result}),201
+    
+    @app.route("/api/authors/<int:uid>")
+    def getauthors(uid):
+        data = DBAPI.GetAuthorByID(uid)
+        return jsonify(data),200
+    
+    @app.route("/api/authors/add", methods=['POST'])
+    def addauthor(author):
+        author = request.get_json()
+        result = DBAPI.InsertAuthor(
+            author["AuthorUID"],
+            author["FirstName"],
+            author["MiddleName"],
+            author["LastName"],
+            author["DateOfBirth"],
+            author["Hometown"],
+            author["Country"])
+        return jsonify({"sucess":True, "result": result}), 201
